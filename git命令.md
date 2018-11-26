7644a6fc68bd79729f9d78772804653d1d25cc2b
Arvinxiaoweihao <1243276550@qq.com>
2018-11-26 10:51:00 +0800

    1

--- git命令.md
+++ git命令.md
@@ -5,3 +5,20 @@ git remote update origin --prune
 ```
 
 这里要注意下，如果你的remote branch不是在origin下，按你得把origin换成你的名字
+
+#### 强制覆盖本地代码（与git远程仓库保持一致）
+
+git强制覆盖：
+
+```sh
+git fetch --all
+git reset --hard origin/master
+git pull
+```
+
+git强制覆盖本地命令（单条执行）：
+
+```sh
+git fetch --all && git reset --hard origin/master && git pull
+```
+
--- shell/startup.sh
+++ shell/startup.sh
@@ -1,8 +1,1 @@
-
 nohup java -jar -XX:PermSize=128M -XX:MaxPermSize=256M pass-admin.jar &
-# nohup java -jar -server -Xms128m -Xmx512m -XX:CompressedClassSpaceSize=128m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m
-# 参数的含义
-# -Xms128m JVM初始分配的堆内存
-# -Xmx512m JVM最大允许分配的堆内存，按需分配
-# -XX:PermSize=128M JVM初始分配的非堆内存
-# -XX:MaxPermSize=512M JVM最大允许分配的非堆内存，按需分配
